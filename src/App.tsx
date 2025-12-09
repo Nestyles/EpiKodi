5aa397064538252ec0e727d68f9369533a636015
Nestyles <tom.sancho@epitech.eu>
2025-12-09 16:25:57 +0100

    [ADD] Chakra UI to our project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
+import { Button, HStack } from "@chakra-ui/react";
+
 import "./App.css";
 
 function App() {

589871f789570dd0e1d14ed3de78c09fc8903331
Nestyles <tom.sancho@epitech.eu>
2025-12-09 16:26:23 +0100

    [FEAT] Set dark mode as default mode

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
+    <Provider defaultTheme="dark">
       <App />
     </Provider>
   </React.StrictMode>,
